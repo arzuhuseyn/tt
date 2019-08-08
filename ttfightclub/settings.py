f8081157ebb861f8699c7162a6c61f2e392d57b6
Arzu Huseynov <arzu.h@kkm.az>
2019-08-08 13:41:30 +0400

    database added

--- ttfightclub/settings.py
+++ ttfightclub/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! 
 SECRET_KEY = 'a1oks2g&5l_jqc$6m=@*q!for&!hef&d=5qdp7#i4rii!_=tat'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'ttfightclub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ttfight',
+        'USER': 'ttuser',
+        'PASSWORD': 'kkmpro001!',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
